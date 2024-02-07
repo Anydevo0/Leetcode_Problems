886f9c76789642f734513a1203fefecfda8346df
DevarshChhatrala <112660656+Anydevo0@users.noreply.github.com>
2024-02-07 15:08:56 +0530

    adding array problem

---
+++ SwapAlternates.cpp
@@ -0,0 +1,37 @@
+//Definition - Swap alternate members of array Ex: arr = [2,3,4,5,6,7] --> arr = [3,2,5,4,7,6]
+
+
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int size) {
+
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+}
+
+void swapAlternate(int arr[], int size) {
+
+    for(int i=0; i<size; i+=2) {
+        if(i+1 < size)
+            swap(arr[i], arr[i+1]);
+    }
+
+}
+
+int main() {
+
+    int even[6] = {5, 4, 2, 1, 8, 7};
+    int odd[5] = {0, 5, 7, 1, 3};
+
+    swapAlternate(even, 6);
+    printArray(even, 6);
+    cout<<endl;
+    swapAlternate(odd, 5);
+    printArray(odd, 5);
+
+
+    return 0;
+}
